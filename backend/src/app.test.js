6f229eb683e099d654d9b2948bc47c1af41843b1
NNNguyen FE <nnn@getachoo.com>
2019-09-04 23:29:00 +0700

    Stakewithus FS Test: - Define APIs: get list, get details, edit validator

--- backend/src/app.test.js
+++ backend/src/app.test.js
@@ -2,19 +2,10 @@ import request from "supertest";
 import app from "./app";
 
 describe("server", () => {
-    describe("/api/users", () => {
-        it("should response with text", () => {
-            request(app)
-                .get("/api/hello")
-                .expect(200)
-                .then(response => {
-                    expect(response.body.express).toBe("Hello From Express");
-                });
-        });
-
+    describe("/api/v1/validators", () => {
         it("should response with json", () => {
             request(app)
-                .get("/api/users")
+                .get("/api/v1/validators")
                 .expect(200)
                 .then(response => {
                     expect(response.body).toBe(response.body);
