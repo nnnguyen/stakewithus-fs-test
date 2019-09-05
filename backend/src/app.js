d979d57534e413455f06294cd6839c19b371b9f8
NNNguyen FE <nnn@getachoo.com>
2019-09-05 15:33:53 +0700

    Stakewithus FS Test: - Define APIs: get list, get details, edit validator

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,15 @@ const cors = require("cors");
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'stake-with-us',
+    password: 'nnnguyen',
+    port: 5432,
+});
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -10,26 +19,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const port = process.env.PORT || 5000;
 
-
-app.get('/api/v1/hello', (req, res) => {
-    res.send({ express: "Hello From Express" });
-});
-
-app.get('/api/v1/validators', (req, res) => {
-    fs.readFile('../data/validators.json', 'utf8', (err, data) =>{
-        res.type('application/json');
-
+/******************************** Do with json data file ********************************/
+var getValidatorListFromFile = (callback) => {
+    fs.readFile('../data/validators.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             throw err;
         }
 
         var json = JSON.parse(data);
-        res.send(json);
+        if(callback) {
+            callback(json);
+        }
     });
-});
+}
 
-app.get('/api/v1/validator/:address', (req, res) => {
+var getValidatorDetailFromFile = (validatorAddress, callback) => {
     fs.readFile('../data/validators.json', 'utf8', (err, data) =>{
         res.type('application/json');
 
@@ -41,14 +46,138 @@ app.get('/api/v1/validator/:address', (req, res) => {
         if(json) {
             var validators = json;
             var validator = validators.find(v => {
-                return v.address === req.params.address;
+                return v.address === validatorAddress;
             });
 
-            res.send(validator);
-        } else {
-            res.send(null);
+            if(callback) {
+                callback(validator);
+            }
         }
 
+    });
+}
+
+var createValidatorListToFile = (callback) => {
+    fs.readFile('../data/validators.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            throw err;
+        }
+
+        var json = JSON.parse(data);
+        if(callback) {
+            callback(json);
+        }
+    });
+}
+
+/******************************** Do with Postgres DB ********************************/
+var getValidatorListFromDB = (callback) => {
+    pool.query('SELECT * FROM validator ORDER BY id ASC',
+        (error, results) => {
+            if (error) {
+                throw error;
+            }
+
+            var validators = results.rows;
+            let ids = validators.map(r => {
+                return parseInt(r.id);
+            });
+
+            pool.query('SELECT * FROM pub_key WHERE validator_id = ANY($1)', [ids],
+                (error, rs) => {
+                    if (error) {
+                        throw error;
+                    }
+                    var pubKeys = rs.rows;
+
+                    validators.forEach(v => {
+                        v.pub_key = {
+                            type: null,
+                            value: null
+                        };
+
+                        var pubKey = pubKeys.find(p => {
+                            console.log(parseInt(p.validator_id) === parseInt(v.id));
+                            return parseInt(p.validator_id) === parseInt(v.id);
+                        });
+                        console.log(pubKey);
+
+                        if(pubKey) {
+                            v.pub_key.type = pubKey.type;
+                            v.pub_key.value = pubKey.value;
+                        }
+                        console.log(v);
+                    });
+
+                    if(callback) {
+                        callback(validators);
+                    }
+                });
+        })
+}
+
+var getValidatorDetailFromDB = (validatorAddress, callback) => {
+    pool.query("SELECT * FROM validator WHERE address = $1", [validatorAddress],
+        (error, results) => {
+            if (error) {
+                throw error;
+            }
+
+            if(results.rows && results.rows.length > 0) {
+                var validator = results.rows[0];
+
+                pool.query('SELECT * FROM pub_key WHERE validator_id = $1', [validator.id],
+                    (error, rs) => {
+                        if (error) {
+                            throw error;
+                        }
+                        
+                        var pubKeys = rs.rows;
+                        validator.pub_key = {
+                            type: null,
+                            value: null
+                        };
+
+                        var pubKey = pubKeys.find(p => {
+                            console.log(parseInt(p.validator_id) === parseInt(validator.id));
+                            return parseInt(p.validator_id) === parseInt(validator.id);
+                        });
+
+                        if(pubKey) {
+                            validator.pub_key.type = pubKey.type;
+                            validator.pub_key.value = pubKey.value;
+                        }
+
+                        if(callback) {
+                            callback(validator);
+                        }
+                    });
+            } else {
+                callback(null);
+            }
+        })
+}
+
+
+/******************************** APIs ********************************/
+app.get('/api/v1/hello', (req, res) => {
+    res.send({ express: "Hello From Express" });
+});
+
+app.get('/api/v1/validators', (req, res) => {
+    res.type('application/json');
+
+    getValidatorListFromDB((results) => {
+        res.json(results);
+    });
+});
+
+app.get('/api/v1/validator/:address', (req, res) => {
+    res.type('application/json');
+
+    getValidatorDetailFromDB(req.params.address, (results) => {
+        res.json(results);
     });
 });
 
@@ -63,7 +192,7 @@ app.post('/api/v1/validator/create', (req, res) => {
         var json = JSON.parse(data);
         if(json) {
             var validators = json;
-            
+
             var existValidator = validators.find(v => {
                 return v.address === req.params.address || v.pub_key.value === req.body.publicKey;
             });
